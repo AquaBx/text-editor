fb7114f4d3540030f56d81b49c720419533cb287
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2024-10-17 15:51:09 +0200

    plus joli

--- TextEditor/tests/test2.cpp
+++ TextEditor/tests/test2.cpp
@@ -13,26 +13,28 @@ void test_case() {
     editor.setTextBuffer("This is a test");
     editor.setClipboard("clipboard");
 
+    editor.keyPressed(false,false,false,SDLK_DOWN);
+
     unsigned long long start = editor.getSelectionStart();
     unsigned long long end = editor.getSelectionEnd();
 
     Tools::assert_equals(start,end, "start et end ne sont pas égaux à l'init");
+    Tools::assert_equals(start ,14, "start n'est pas à la fin de la string à l'init");
 
-    editor.keyPressed(false,false,false,SDLK_a);
+    editor.keyPressed(false,false,false,SDLK_RIGHT);
 
-    Tools::assert_equals(editor.getSelectionStart(),start+1, "");
-    Tools::assert_equals(editor.getSelectionEnd(),end+1,"");
-
-    Tools::assert_equals(editor.getTextBuffer(),"aThis is a test","");
-
+    Tools::assert_equals(editor.getSelectionStart(),start, "start a bougé malgré qu'il soit à la fin de la string");
+    Tools::assert_equals(editor.getSelectionEnd(),end,"end a bougé malgré qu'il soit à la fin de la string");
 }
+
 int main()
 {
     int result;
-    std::cout << "--------- test1 --------" << std::endl;
+
+    std::cout << "--------- test --------" << std::endl;
     result = Tools::boubiboulga(test_case);
     std::cout << "Error Code : " << result << std::endl;
-    std::cout << "---- test1 finished ----" << std::endl;
+    std::cout << "---- test finished ----" << std::endl;
 
     return result;
 }
