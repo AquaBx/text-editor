1c78820d0589f3a92794a0841015e3c3567dc676
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2024-10-19 13:10:24 +0200

    tests

--- TextEditor/tests/test1.cpp
+++ TextEditor/tests/test1.cpp
@@ -8,7 +8,8 @@
 
 #include "tools.h"
 
-void test_case() {
+void test_case()
+{
     TextEditor editor;
     editor.setTextBuffer("This is a test");
     editor.setClipboard("clipboard");
@@ -16,23 +17,24 @@ void test_case() {
     unsigned long long start = editor.getSelectionStart();
     unsigned long long end = editor.getSelectionEnd();
 
-    Tools::assert_equals(start,end, "start et end ne sont pas égaux à l'init");
+    Tools::assert_equals(start, end, "start et end ne sont pas égaux à l'init");
 
-    editor.keyPressed(false,false,false,'a');
+    editor.keyPressed(false, false, false, 'a');
 
-    Tools::assert_equals(editor.getSelectionStart(),start+1, "Position du curseur pas bon");
-    Tools::assert_equals(editor.getSelectionEnd(),end+1,"Position du curseur pas bon");
+    Tools::assert_equals(editor.getSelectionStart(), start + 1, "Position du curseur pas bon");
+    Tools::assert_equals(editor.getSelectionEnd(), end + 1, "Position du curseur pas bon");
 
-    Tools::assert_equals(editor.getTextBuffer(),std::string("aThis is a test"),"Textepas bon");
-
+    Tools::assert_equals(editor.getTextBuffer(), std::string("aThis is a test"), "Textepas bon");
 }
+
 int main()
 {
     int result;
-    std::cout << "--------- test1 --------" << std::endl;
+    std::cout << "--------- test --------" << std::endl;
+    std::cout << "Description : ajouter un caractère ajoute la lettre et décale de 1 notre curseur" << std::endl;
     result = Tools::test(test_case);
     std::cout << "Error Code : " << result << std::endl;
-    std::cout << "---- test1 finished ----" << std::endl;
+    std::cout << "---- test finished ----" << std::endl;
 
     return result;
 }
--- TextEditor/tests/test2.cpp
+++ TextEditor/tests/test2.cpp
@@ -8,23 +8,24 @@
 
 #include "tools.h"
 
-void test_case() {
+void test_case()
+{
     TextEditor editor;
     editor.setTextBuffer("This is a test");
     editor.setClipboard("clipboard");
 
-    editor.keyPressed(false,false,false,SDLK_DOWN);
+    editor.keyPressed(false, false, false, SDLK_DOWN);
 
     unsigned long long start = editor.getSelectionStart();
     unsigned long long end = editor.getSelectionEnd();
 
-    Tools::assert_equals(start,end, "start et end ne sont pas égaux à l'init");
-    Tools::assert_equals(start ,(unsigned long long)14, "start n'est pas à la fin de la string à l'init");
+    Tools::assert_equals(start, end, "start et end ne sont pas égaux à l'init");
+    Tools::assert_equals(start, static_cast<unsigned long long>(14), "start n'est pas à la fin de la string à l'init");
 
-    editor.keyPressed(false,false,false,SDLK_RIGHT);
+    editor.keyPressed(false, false, false, SDLK_RIGHT);
 
-    Tools::assert_equals(editor.getSelectionStart(),start, "start a bougé malgré qu'il soit à la fin de la string");
-    Tools::assert_equals(editor.getSelectionEnd(),end,"end a bougé malgré qu'il soit à la fin de la string");
+    Tools::assert_equals(editor.getSelectionStart(), start, "start a bougé malgré qu'il soit à la fin de la string");
+    Tools::assert_equals(editor.getSelectionEnd(), end, "end a bougé malgré qu'il soit à la fin de la string");
 }
 
 int main()
@@ -32,6 +33,7 @@ int main()
     int result;
 
     std::cout << "--------- test --------" << std::endl;
+    std::cout << "Description : Va à la fin du buffer, et essaie de se déplacer à droite" << std::endl;
     result = Tools::test(test_case);
     std::cout << "Error Code : " << result << std::endl;
     std::cout << "---- test finished ----" << std::endl;
