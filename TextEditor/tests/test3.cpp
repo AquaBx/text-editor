377070e7826e77da31e186911a72399c466b08f0
celiajosso <70684974+celiajosso@users.noreply.github.com>
2024-10-21 12:56:03 +0200

    amélioration des tests

--- TextEditor/tests/test3.cpp
+++ TextEditor/tests/test3.cpp
@@ -15,15 +15,19 @@ void test_case()
     editor.setClipboard("clipboard");
 
     editor.keyPressed(false, false, false, SDLK_RIGHT);
-
     unsigned long long start = editor.getSelectionStart();
     unsigned long long end = editor.getSelectionEnd();
 
-    editor.keyPressed(false, false, true, SDLK_RIGHT);
-    editor.keyPressed(false, false, true, SDLK_LEFT);
+    Tools::assert_equals(start, (unsigned long long) 1, "1 : Le curseur n'est pas en position 1 après le déplacement.");
+    Tools::assert_equals(end, (unsigned long long) 1, "2 : La fin de la sélection n'est pas en position 1 après le déplacement.");
 
-    Tools::assert_equals(editor.getSelectionStart(), start, "Pas le bon comportement");
-    Tools::assert_equals(editor.getSelectionEnd(), end, "Pas le bon comportement");
+    editor.keyPressed(false, false, true, SDLK_RIGHT);
+    Tools::assert_equals(editor.getSelectionStart(), start, "3 : La sélection commence à la mauvaise position.");
+    Tools::assert_equals(editor.getSelectionEnd(), (unsigned long long) 2, "4 : La fin de la sélection n'est pas correcte après sélection.");
+
+    editor.keyPressed(false, false, true, SDLK_LEFT);
+    Tools::assert_equals(editor.getSelectionStart(), start, "5 : La sélection n'a pas correctement désélectionné le caractère.");
+    Tools::assert_equals(editor.getSelectionEnd(), end, "6 : La fin de la sélection après désélection n'est pas correcte.");
 }
 
 int main()
