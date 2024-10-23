a01487708538a810bc3ec9d1dab48b7c2e96b159
celiajosso <70684974+celiajosso@users.noreply.github.com>
2024-10-23 18:18:32 +0200

    amélioration sorties test4

--- TextEditor/tests/test4.cpp
+++ TextEditor/tests/test4.cpp
@@ -2,7 +2,6 @@
 // Created by tomch on 17/10/2024.
 //
 #include <cassert>
-#include <iostream>
 
 #include "../src/TextEditor/TextEditor.h"
 
@@ -14,19 +13,25 @@ void test_case()
     editor.setTextBuffer("This is a test");
     editor.setClipboard("clipboard");
 
+    std::cout << "Selection start before Ctrl+A: " << editor.getSelectionStart() << std::endl;
+    std::cout << "Selection end before Ctrl+A: " << editor.getSelectionEnd() << std::endl;
+
     editor.keyPressed(true, false, false, 'a');
+
+    std::cout << "Selection start after Ctrl+A: " << editor.getSelectionStart() << std::endl;
+    std::cout << "Selection end after Ctrl+A: " << editor.getSelectionEnd() << std::endl;
 
     unsigned long long start = editor.getSelectionStart();
     unsigned long long end = editor.getSelectionEnd();
 
     Tools::assert_equals(start, (unsigned long long) 0, "1 : La sélection ne commence pas au début du texte. Actual: " + std::to_string(start) + "; Expected: " + std::to_string(0));
-    Tools::assert_equals(end, editor.getTextBuffer().length(), "2 : La sélection ne couvre pas tout le texte.");
+    Tools::assert_equals(end, editor.getTextBuffer().length(), "2 : La sélection ne couvre pas tout le texte. Actual: " + std::to_string(end) + "; Expected: " + std::to_string(editor.getTextBuffer().length()));
 
     editor.keyPressed(true, false, false, 'c');
 
-    Tools::assert_equals(editor.getClipboard(), editor.getTextBuffer(), "3 : Le presse-papier ne contient pas tout le texte.");
-    Tools::assert_equals(editor.getSelectionStart(), start, "4 : Le début de la sélection a changé après la copie.");
-    Tools::assert_equals(editor.getSelectionEnd(), end, "5 : La fin de la sélection a changé après la copie.");
+    Tools::assert_equals(editor.getClipboard(), editor.getTextBuffer(), "3 : Le presse-papier ne contient pas tout le texte. Actual: " + editor.getClipboard() + "; Expected: " + editor.getTextBuffer());
+    Tools::assert_equals(editor.getSelectionStart(), start, "4 : Le début de la sélection a changé après la copie. Actual: " + std::to_string(start) + "; Expected: " + std::to_string(editor.getSelectionStart()));
+    Tools::assert_equals(editor.getSelectionEnd(), end, "5 : La fin de la sélection a changé après la copie. Actual: " + std::to_string(end) + "; Expected: " + std::to_string(editor.getSelectionStart()));
 }
 
 int main()
